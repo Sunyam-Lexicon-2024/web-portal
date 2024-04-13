11d0213054021bd3902d4d2be6d8b48e3213b904
suny-am <visualarea.1@gmail.com>
2024-04-13 21:10:26 +0200

    update devcontainer name

--- data_import/importGithubData.zsh
+++ data_import/importGithubData.zsh
@@ -1,5 +1,5 @@
 #! /usr/bin/zsh
-token=$(echo $TOKEN | base64 -d)
+token=$(echo $GITHUB_ADMIN_ACCESS | base64 -d)
 
 query=$(cat query.gql | tr -d '\n')
 variables=$(cat variables.gql | tr -d '\n')
@@ -9,9 +9,6 @@ data=$(jq -n \
            '{ query: $q , variables: $v}')
 
 # Send query request
-response=$(curl -s \
-  -H "Authorization: bearer $token" \
-  --data "$data" \
-  https://api.github.com/graphql)
-  
+response=$(curl -s -H "Authorization: bearer $token" -d "$data" https://api.github.com/graphql)
+
 echo $response | jq '.data.search | to_entries | .[1].value | map(.repository = .node | del(.node)) ' > ../public/repositories.json
